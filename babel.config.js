a59bd253030efac0a6416b763b5ca0014947d758
rifandani <ipandani2505@gmail.com>
2023-09-29 15:06:17 +0700

    chore: uninstall @babel/plugin-proposal-export-namespace-from, new launch json

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin'],
+    plugins: [
+      // '@babel/plugin-proposal-export-namespace-from', // It is now included in babel-preset-expo in Expo SDK 49
+      'react-native-reanimated/plugin',
+    ],
   };
 };
